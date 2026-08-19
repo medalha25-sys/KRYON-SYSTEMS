ee630c1719a4f546336586f3ec5b5fbee3a7d2a7
Wesley Jesus <medalha25@gmail.com>
2026-08-19 14:43:32 -0300

    Configure auto-update and cache purging for PWA and service worker

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,15 @@ import App from './App.jsx'
 import ErrorBoundary from './ErrorBoundary.jsx'
 import './index.css'
 
+// Atualiza o Service Worker e limpa cache antigo instantaneamente
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.getRegistrations().then((registrations) => {
+    for (const registration of registrations) {
+      registration.update();
+    }
+  });
+}
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ErrorBoundary>
